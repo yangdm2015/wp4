811f2891a1a863f9a644d3afca4339d57a3ce453
yangshan08 <yangshan08@meituan.com>
2020-07-29 08:55:04 +0800

    init

---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+var path = require("path");
+var webpack = require("webpack");
+const WriteFilePlugin = require("write-file-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const VueLoaderPlugin = require("vue-loader/lib/plugin");
+
+module.exports = {
+  entry: "./src/main.js", // 项目的入口文件，webpack会从main.js开始，把所有依赖的js都加载打包
+  output: {
+    path: path.resolve(__dirname, "./dist"), // 项目的打包文件路径
+    publicPath: "/dist/", // 通过devServer访问路径
+    filename: "build.js", // 打包后的文件名
+  },
+  devServer: {
+    // historyApiFallback: true, //historyApiFallback设置为true那么所有的路径都执行index.html。
+    // // overlay: true, // 将错误显示在html之上
+  },
+  resolve: {
+    alias: {
+      vue$: "vue/dist/vue.esm.js",
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      // ... other rules
+      {
+        test: /\.vue$/,
+        loader: "vue-loader",
+      },
+    ],
+  },
+  plugins: [
+    new WriteFilePlugin(),
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: "index.html",
+      inject: true,
+    }),
+    new VueLoaderPlugin(),
+  ],
+};
