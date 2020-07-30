dd7dbdddf226b0e0ab397fae8d704d0537537576
杨杉 <yangshan08@meituan.com>
2020-07-30 16:16:50 +0800

    App.vue init

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+     <div class="about">
+    <h1>This is an about page</h1>
+    <p>{{ word }}</p>
+  </div>
+</template>    
+
+<script lang="ts">
+import Vue from 'vue'
+import Component from 'vue-class-component';
+
+@Component
+export default class App extends Vue{
+     word: string = "天涯共此时";
+}
+</script>
