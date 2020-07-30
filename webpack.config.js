e04e91cf58c5b2c8dc2e419ecec1baa720b269e5
杨杉 <yangshan08@meituan.com>
2020-07-30 14:34:44 +0800

    add Vue

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const VueLoaderPlugin = require("vue-loader/lib/plugin");
 
 module.exports = {
+  mode:'development',
   entry: "./src/main.js", // 项目的入口文件，webpack会从main.js开始，把所有依赖的js都加载打包
   output: {
     path: path.resolve(__dirname, "./dist"), // 项目的打包文件路径
@@ -12,8 +13,10 @@ module.exports = {
     filename: "build.js", // 打包后的文件名
   },
   devServer: {
-    // historyApiFallback: true, //historyApiFallback设置为true那么所有的路径都执行index.html。
-    // // overlay: true, // 将错误显示在html之上
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 9000,
+    overlay:true
   },
   resolve: {
     alias: {
@@ -36,7 +39,6 @@ module.exports = {
           },
         },
       },
-      // ... other rules
       {
         test: /\.vue$/,
         loader: "vue-loader",
@@ -44,12 +46,19 @@ module.exports = {
     ],
   },
   plugins: [
-    new WriteFilePlugin(),
-    new HtmlWebpackPlugin({
-      filename: "index.html",
-      template: "index.html",
-      inject: true,
+    new webpack.SourceMapDevToolPlugin({
+      filename: '[file].map',
+    }),
+    new webpack.DefinePlugin({
+      'process.env': { NODE_ENV:'"development"'}
     }),
     new VueLoaderPlugin(),
+    new WriteFilePlugin(),
+    new HtmlWebpackPlugin({
+      // filename: "index.html",
+      // template: "index.html",
+      template: "./src/template.html",
+      inject: true,
+    }),
   ],
 };
