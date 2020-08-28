5e2f8f0951f6384b96227a754ebc6edec7c7c221
杨杉 <yangshan08@meituan.com>
2020-08-28 18:26:28 +0800

    typescript应用

--- src/pages/Table/index.vue
+++ src/pages/Table/index.vue
@@ -1,10 +1,20 @@
 <template>
     <div>
-        <h1>我是表格wraper </h1>
+        <h1>我是表格wraper1</h1>
         <el-button @click="changeShow">点我{{show?'隐藏':'显示'}}</el-button>
         <div v-if="show">
             <tb></tb>
         </div>
+        <el-select v-model="s">
+            <el-option
+                v-for="item in options"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value"
+            >
+                <!-- <span><span style="opacity:0">%20%20%20</span>{{item.label}}</span> -->
+            </el-option>
+        </el-select>
     </div>
 </template>
 <script lang="ts">
@@ -16,6 +26,10 @@ interface Person {
     date: string;
     address: string;
 }
+interface option {
+    value: string;
+    label: string;
+}
 
 @Component({
     components: {
@@ -24,8 +38,13 @@ interface Person {
 })
 export default class tbPage extends Vue {
     show: Boolean = false;
+    s: String = "1";
+    options: Array<option> = [
+        { value: "1", label: "<span>sds</span>我说的是" }
+    ];
     changeShow() {
         this.show = !this.show;
     }
+   
 }
 </script>
--- src/pages/Test/index.vue
+++ src/pages/Test/index.vue
@@ -1,11 +1,25 @@
 <template>
     <div>
         test1
+        {{modeByer}}
+        <el-button @click="c">改变</el-button>
     </div>
 </template>
 <script lang="ts">
 import Vue from 'vue'
-export default Vue.extend({
-    
-})
+import Component from "vue-class-component";
+function modeByer(test:test) {
+    return test.s + "haha";
+}
+@Component
+export default class test extends  Vue{
+    s: String = "1";
+    c(){
+        // console.log('this=',this)
+        this.s=''+Number(this.s)*2
+    }
+    get modeByer() {
+        return modeByer(this);
+    }
+}
 </script>
